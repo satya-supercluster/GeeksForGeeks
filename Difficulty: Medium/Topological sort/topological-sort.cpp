be7edf731a7b0843e053c86481724f4f07265406
Satyam Gupta <120209121+satya-supercluster@users.noreply.github.com>
2025-04-06 22:16:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Topological sort/topological-sort.cpp
@@ -0,0 +1,86 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    vector<int> topoSort(int V, vector<vector<int>>& edges) {
+        // code here
+        vector<int> topo,vis(V,0);
+        vector<vector<int>> adj(V);
+        for(auto&i:edges){
+            adj[i[0]].push_back(i[1]);
+        }
+        function<void(int)> dfs=[&](int u)->void{
+            vis[u]=true;
+            for(auto&v:adj[u]){
+                if(!vis[v]){
+                    dfs(v);
+                }
+            }
+            topo.push_back(u);
+        };
+        for(int i{};i<V;i++){
+            if(!vis[i]) dfs(i);
+        }
+        reverse(topo.begin(),topo.end());
+        return topo;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int check(int V, vector<int> &res, vector<vector<int>> adj) {
+
+    if (V != res.size())
+        return 0;
+
+    vector<int> map(V, -1);
+    for (int i = 0; i < V; i++) {
+        map[res[i]] = i;
+    }
+    for (int i = 0; i < V; i++) {
+        for (int v : adj[i]) {
+            if (map[i] > map[v])
+                return 0;
+        }
+    }
+    return 1;
+}
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int V, E;
+        cin >> V >> E;
+
+        int x = V;
+        vector<vector<int>> adj(V);
+        vector<vector<int>> edges;
+
+        for (int i = 0; i < E; i++) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+            edges.push_back({u, v});
+        }
+
+        Solution obj;
+        vector<int> res = obj.topoSort(V, edges);
+        bool ans = check(x, res, adj);
+        if (ans)
+            cout << "true\n";
+        else
+            cout << "false\n";
+        cout << "~"
+             << "\n";
+    }
+
+    return 0;
+}
+// } Driver Code Ends
